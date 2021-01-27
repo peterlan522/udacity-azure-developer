4a16afca47e30e5550f472d99101601f099d4e5b
xl8286 <xl8286@att.com>
2021-01-26 21:42:17 -0600

    exercise on msal python api

--- oauth-msal-starter/FlaskExercise/views.py
+++ oauth-msal-starter/FlaskExercise/views.py
@@ -32,7 +32,8 @@ def logout():
         session.clear()
         # TODO: Also logout from your tenant's web session
         #   And make sure to redirect from there back to the login page
-        pass
+        return redirect(Config.AUTHORITY + '/oauth2/v2.0/logout' +
+     '?post_logout_redirect_uri=' + url_for('login', _external=True))
 
     return redirect(url_for('login'))
 
@@ -74,10 +75,13 @@ def _save_cache(cache):
 
 
 def _build_msal_app(cache=None, authority=None):
-    # TODO: Create and return a Confidential Client Application from msal
-    return None
+    return msal.ConfidentialClientApplication(Config.CLIENT_ID, authority=authority or Config.AUTHORITY,
+                                              client_credential=Config.CLIENT_SECRET, token_cache=cache)
 
 
 def _build_auth_url(authority=None, scopes=None, state=None):
-    # TODO: Get the authorization request URL from a built msal app, and return it
-    return None
+    return _build_msal_app(authority=authority).get_authorization_request_url(
+        scopes or [],
+        state=state or str(uuid.uuid4()),
+        redirect_uri=url_for('authorized', _external=True, _scheme='https')
+    )
--- oauth-msal-starter/config.py
+++ oauth-msal-starter/config.py
@@ -1,17 +1,14 @@
 class Config(object):
     # In a production app, store this instead in KeyVault or an environment variable
-    # TODO: Enter your client secret from Azure AD below
-    CLIENT_SECRET = "{YOUR-CLIENT-SECRET}" 
+    CLIENT_SECRET = "5FFovNDa-qH.Vxyrlrfd-b30_fr5lX.c90"
 
     AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
-    # TODO: Enter your application client ID here
-    CLIENT_ID = "{YOUR-APP-CLIENT-ID}"
+    CLIENT_ID = "c8de2b63-48fa-4110-8f19-516ec9bda886"
 
-    # TODO: Enter the redirect path you want to use for OAuth requests
     #   Note that this will be the end of the URI entered back in Azure AD
-    REDIRECT_PATH = "/{YOUR-REDIRECT-PATH}"  # Used to form an absolute URL, 
+    REDIRECT_PATH = "/getToken"  # Used to form an absolute URL,
         # which must match your app's redirect_uri set in AAD
 
     # You can find the proper permission names from this document
